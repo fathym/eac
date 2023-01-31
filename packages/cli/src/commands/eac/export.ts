343954bfc5781a36a43464936a9390f2e0e07171
mcgear <michael.gearhardt@gmail.com>
2023-01-31 07:33:20 -0700

    Export to file support

--- packages/cli/src/commands/eac/export.ts
+++ packages/cli/src/commands/eac/export.ts
@@ -1,4 +1,4 @@
-import {} from '@oclif/core';
+import { Flags } from '@oclif/core';
 import { ListrTask } from 'listr2';
 import { EnterpriseAsCode } from '@semanticjs/common';
 import { FathymCommand } from '../../common/fathym-command';
@@ -11,6 +11,8 @@ import {
   loadEaCTask,
 } from '../../common/core-helpers';
 import { withEaCDraft } from '../../common/eac-services';
+import { mkdir, pathExists, writeJson } from 'fs-extra';
+import path from 'node:path';
 
 interface ExportTaskContext
   extends FathymTaskContext,
@@ -22,20 +24,43 @@ export default class Export extends FathymCommand<ExportTaskContext> {
 
   static examples = ['<%= config.bin %> <%= command.id %>'];
 
-  static flags = {};
+  static flags = {
+    file: Flags.string({
+      char: 'f',
+      description: 'File path where the export should be written',
+    }),
+  };
 
   static args = [];
 
   static title = 'EaC Export';
 
   protected async loadTasks(): Promise<ListrTask<ExportTaskContext>[]> {
+    const { flags } = await this.parse(Export);
+
+    const { file } = flags;
+
     return [
       ensureActiveEnterprise(this.config.configDir),
       loadEaCTask(this.config.configDir),
       {
         title: `Exporting EaC`,
         task: async (ctx, task) => {
-          ctx.Fathym.Result = JSON.stringify(ctx.EaC, undefined, 2);
+          if (file) {
+            const dir = path.dirname(file);
+
+            const exists = await pathExists(dir);
+
+            if (!exists) {
+              await mkdir(dir, { recursive: true });
+            }
+
+            await writeJson(file, ctx.EaC, { spaces: 2 });
+
+            task.title = `Exported EaC to ${file}`;
+          } else {
+            ctx.Fathym.Result = JSON.stringify(ctx.EaC, undefined, 2);
+          }
         },
       },
     ];
